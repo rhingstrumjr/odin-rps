91563cd199a31a5f9abd61172897e228b410021e
Rob Hingstrum <rhingstrumjr@gmail.com>
2021-12-12 07:14:30 -0600

    Refactor play round and added to buttons

--- script.js
+++ script.js
@@ -5,50 +5,47 @@ const computerPlay = function () {
     return options[random]
 }
 
-const playerSelection = function () {
-    let input = prompt("Rock, paper, or scissors?").toLowerCase().trim()
-    if (input == 'rock' || input == 'paper' || input == 'scissors') {
-        return input
-    } else {
-        console.log('Choice must be rock, paper, or scissors')
-        playerSelection()
-    }
-}
+// function game() {
+//     let userScore = 0
+//     let compScore = 0
+//     while (userScore < 3 && compScore < 3) {
+//         let result = playRound(playerSelection(), computerPlay())
+//         if (result == "You win!") {
+//             userScore++
+//         }
+//         if (result == "Computer wins") {
+//             compScore++
+//         }
+//         console.log(`Your score: ${userScore} \n Computer score: ${compScore}`)
+//     }
+//     if (userScore === 3) {
+//         return "You win! :)"
+//     } else {
+//         return "You lose. :("
+//     }
+// }
 
-function playRound(userPlay, cPlay) {
-    if (cPlay == userPlay) {
-        return "Tie!"
-    }
-    if (userPlay == 'rock' && cPlay == 'scissors') {
-        return "You win!"
-    }
-    if (userPlay == 'scissors' && cPlay == 'paper') {
-        return "You win!"
-    }
-    if (userPlay == 'paper' && cPlay == 'rock') {
-        return "You win!"
-    }
-    return "Computer wins"
-}
-
-function game() {
-    let userScore = 0
-    let compScore = 0
-    while (userScore < 3 && compScore < 3) {
-        let result = playRound(playerSelection(), computerPlay())
-        if (result == "You win!") {
-            userScore++
+const game = {
+    humanScore: 0,
+    compScore: 0,
+    playRound(selection) {
+        const computerSelection = computerPlay();
+        if (computerSelection == selection) {
+            return "Tie!"
         }
-        if (result == "Computer wins") {
-            compScore++
+        if (selection == 'rock' && cPlay == 'scissors') {
+            this.humanScore++
+            return "You win!"
         }
-        console.log(`Your score: ${userScore} \n Computer score: ${compScore}`)
-    }
-    if (userScore === 3) {
-        return "You win! :)"
-    } else {
-        return "You lose. :("
+        if (selection == 'scissors' && cPlay == 'paper') {
+            this.humanScore++
+            return "You win!"
+        }
+        if (selection == 'paper' && cPlay == 'rock') {
+            this.humanScore++
+            return "You win!"
+        }
+        this.compScore++
+        return "Computer wins"
     }
 }
-
-console.log(game())
