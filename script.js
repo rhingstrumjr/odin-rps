1af59032dd368f8b6d439dc347ee4e86ddff451e
Rob Hingstrum <rhingstrumjr@gmail.com>
2021-12-12 07:45:22 -0600

    Delete commented out code

--- script.js
+++ script.js
@@ -5,26 +5,6 @@ const computerPlay = function () {
     return options[random]
 }
 
-// function game() {
-//     let userScore = 0
-//     let compScore = 0
-//     while (userScore < 3 && compScore < 3) {
-//         let result = playRound(playerSelection(), computerPlay())
-//         if (result == "You win!") {
-//             userScore++
-//         }
-//         if (result == "Computer wins") {
-//             compScore++
-//         }
-//         console.log(`Your score: ${userScore} \n Computer score: ${compScore}`)
-//     }
-//     if (userScore === 3) {
-//         return "You win! :)"
-//     } else {
-//         return "You lose. :("
-//     }
-// }
-
 const game = {
     humanScore: 0,
     compScore: 0,
